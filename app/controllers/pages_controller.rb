03090d01e9593dec6b4221e7465fc262d8dcbd81
Shalin Maria <jinx.shalinaria@gmail.com>
2024-04-22 13:51:03 -0600

    Add Shalin page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
   end
+  def Shalin_Maria
+    render :Shalin_Maria
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+  get 'Shalin_Maria', to: 'pages#ShalinMaria', as: 'Shalin_Maria'
 end
